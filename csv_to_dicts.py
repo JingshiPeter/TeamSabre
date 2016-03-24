da970156a32c71eb60addb276e36faff09ca988f
Jingshi Sun <hanzi@utexas.edu>
2016-03-24 15:59:33 -0500

    new file

---
+++ csv_to_dicts.py
@@ -0,0 +1,48 @@
+import pandas
+
+#DEFINE GLOBAL NAMES HERE
+CREWDATA_CSV = 'CrewData.csv'
+
+crew_df = pandas.read_csv(CREWDATA_CSV)
+
+def get_base_dic():
+	# return a base dictionary
+	# example : 
+	# 	bases = {
+
+	# 'b1' : {'900488', '900421'},
+	# 'b2' : {'900201', '900424'}
+
+	# }
+
+def get_rank_dic():
+	# return a rank dictionary
+	# example : 
+	# ranks = {
+
+	# 'cpt' : {'900201','900488', '900421'},
+	# 'fo' : {'900424'}
+
+	# }
+
+def get_fleet_dic():
+	# return a fleet dictionary
+	# example :
+	# fleets = {
+
+	# 'a330' : {'900201', '900421'},
+	# 'a320' : {'900488', '900424'}
+
+	# }
+def get_from_fleet_dic():
+
+def get_to_fleet_dic():
+
+def get_from_rank_dic():
+
+def get_to_rank_dic():
+
+def get_from_base_dic():
+
+def get_to_base_dic():
+	
