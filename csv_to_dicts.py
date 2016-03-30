607ac510349b6d508b293c3a5efda3a37d928775
Jingshi Sun <hanzi@utexas.edu>
2016-03-29 22:15:36 -0500

    add demanddata

--- csv_to_dicts.py
+++ csv_to_dicts.py
@@ -6,24 +6,28 @@ CREWDATA_CSV = 'CrewData.csv'
 crew_df = pandas.read_csv(CREWDATA_CSV)
 
 def get_base_dic():
+	###finished
 	# return a base dictionary
 	# example : 
 	# 	bases = {
 
 	# 'b1' : {'900488', '900421'},
 	# 'b2' : {'900201', '900424'}
+    
+    #  }
 	
 	b1_ids = crew_df[crew_df.Current_Base == 1]['Crew_ID']
 	b2_ids = crew_df[crew_df.Current_Base == 2]['Crew_ID']
-	print "number of pilots in base 1 is " + str(len(b1_ids))
-	print "number of pilots in base 2 is " + str(len(b2_ids))
-	base_dict = {
+	# print "number of pilots in base 1 is " + str(len(b1_ids))
+	# print "number of pilots in base 2 is " + str(len(b2_ids))
+	base_dic = {
 	    "B1" : set(b1_ids),
 	    "B2" : set(b2_ids),
 	    }
-	return base_dict
+	return base_dic
 
 def get_rank_dic():
+	###rank
 	# return a rank dictionary
 	# example : 
 	# ranks = {
@@ -34,8 +38,8 @@ def get_rank_dic():
 	# }
 	cpt_ids = crew_df[crew_df.Rank == "CPT"]['Crew_ID']
 	fo_ids = crew_df[crew_df.Rank == "FO"]['Crew_ID']
-	print "number of first captains is " + str(len(cpt_ids))
-	print "number of first officers is " + str(len(fo_ids))
+	# print "number of first captains is " + str(len(cpt_ids))
+	# print "number of first officers is " + str(len(fo_ids))
 	rank_dic = {
 	    "CPT" : set(cpt_ids),
 	    "FO" : set(fo_ids)
@@ -43,6 +47,7 @@ def get_rank_dic():
 	return rank_dic
 
 def get_fleet_dic():
+	###finished
 	# return a fleet dictionary
 	# example :
 	# fleets = {
@@ -59,16 +64,23 @@ def get_fleet_dic():
 	    "A330" : set(A330_ids),
 	    }
 	print fleet_dic
-	return 0
+	return fleet_dic
 
 def get_orig_fleet_dic():
-	return 0
+	###finished
+	orig_fleet_dic = {
+		"A320" : set(crew_df[crew_df.Bid_FleetChange.notnull()][crew_df.Cur_Fleet == "A320"]['Crew_ID']),
+		"A330" : {}
+	}
+	print orig_fleet_dic
+	return orig_fleet_dic
 
 def get_future_fleet_dic():
+	###finished
 	fleet_change_future_A320_ids = crew_df[crew_df.Bid_FleetChange == "A320"]['Crew_ID']
 	fleet_change_future_A330_ids = crew_df[crew_df.Bid_FleetChange == "A330"]['Crew_ID']
-	print "number of pilots who bid fleetchange future A320 is " + str(len(fleet_change_future_A320_ids))
-	print "number of pilots who bid fleetchange future A330 is " + str(len(fleet_change_future_A330_ids))
+	# print "number of pilots who bid fleetchange future A320 is " + str(len(fleet_change_future_A320_ids))
+	# print "number of pilots who bid fleetchange future A330 is " + str(len(fleet_change_future_A330_ids))
 	future_fleet_dic = {
 	    "A320" : set(fleet_change_future_A320_ids),
 	    "A330" : set(fleet_change_future_A330_ids),
@@ -77,10 +89,22 @@ def get_future_fleet_dic():
 	return future_fleet_dic
 
 def get_orig_rank_dic():
-	return 0
+	###finished
+	orig_rank_dic = {
+		"FO" : set(crew_df[crew_df.Bid_RankChange.notnull()][crew_df.Rank == "FO"]['Crew_ID']),
+		"CPT" : {}
+	}
+	print orig_rank_dic
+	return orig_rank_dic
 
 def get_future_rank_dic():
-	return 0
+	###finished
+	future_rank_dic = {
+		"CPT" : set(crew_df[crew_df.Bid_RankChange.notnull()][crew_df.Rank == "FO"]['Crew_ID']),
+		"FO" : {}
+	}
+	print future_rank_dic
+	return future_rank_dic
 
 def get_orig_base_dic():
     b1_o_ids = crew_df[(crew_df.Bid_BaseChange == 1)]['Crew_ID']
@@ -105,14 +129,26 @@ def get_demand(base, fleet, rank, week):
 	# return the demand at B1, A330, FO of week 0
 	return 0
 
-def get_future_position(base, fleet, rank):
-	# return non-fix group pilot ids whose future position is input
-	return 0
+orig_rank_dic = get_orig_rank_dic()
+orig_fleet_dic = get_orig_fleet_dic()
+orig_base_dic = get_orig_base_dic()
+
+future_rank_dic = get_future_rank_dic()
+future_fleet_dic = get_future_fleet_dic()
+future_base_dic = get_future_base_dic()
 
 def get_orig_position(base, fleet, rank):
 	# return non-fix group pilot ids whose orig position is input
-	return 0
+	return set(orig_base_dic[base] and orig_fleet_dic[fleet] and orig_rank_dic[rank])
 
+def get_future_position(base, fleet, rank):
+	# return non-fix group pilot ids whose future position is input
+	return set(future_base_dic[base] and future_fleet_dic[fleet] and future_rank_dic[rank])
+
+
+base_dic = get_base_dic()
+rank_dic = get_rank_dic()
+fleet_dic = get_fleet_dic()
 def get_position(base, fleet, rank):
 	# return fix group pilot ids whose position is input
-	return 0
+	return set(base_dic[base] and fleet_dic[fleet] and rank_dic[rank])
