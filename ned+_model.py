11cf4721ed50468567aceb711f5aa9daf2decc83
MridulaMaddukuri <mridulamaddukuri@utexas.edu>
2016-04-05 16:07:37 -0500

    refined results

--- ned+_model.py
+++ ned+_model.py
@@ -347,8 +347,11 @@ for pilott in list_nfPilots:
 		for base in list(model.base.value):
 			for rank in list(model.rank.value):
 				for fleet in list(model.fleet.value):
-					print str(pilott) + "__" + str(rank) + '__'+ str(fleet) + '__' + str(base) + '_week' + str(i) 
-					print model.Y[pilott,rank,fleet,base,i].value 
+					if model.Y[pilott,rank,fleet,base,i].value == 0.0 or model.Y[pilott,rank,fleet,base,i].value == 1.0:
+						print str(pilott) + "__" + str(rank) + '__'+ str(fleet) + '__' + str(base) + '_week' + str(i) 
+						print model.Y[pilott,rank,fleet,base,i].value 
+					
+
 
 
 # record the transition in each week
