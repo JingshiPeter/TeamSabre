21a7f31c413285ba5d5c12f5809f17d2abe1c7f1
Jingshi Sun <hanzi@utexas.edu>
2016-04-05 17:13:03 -0500

    automatic message

--- ned+_model.py
+++ ned+_model.py
@@ -318,7 +318,7 @@ def get_slot(t):
 
  #vacation constraint. -vacation. pilot <= slot. 
 def pilot_vacation_slot_exceed(model, t):
-	return pe.summation(model.V, index = [(p,t) for t == t ]) <= get_slot(t)  
+	return pe.summation(model.V, index = [(p,t) for (p,t) in model.pilots*model.time if t == t]) <= get_slot(t)  
 model.pilot_vacation_slot_exceed = pe.Constraint(model.time, rule = pilot_vacation_slot_exceed)
 
 # def trainer_binding_rule(model, b, t):
