384e753224d10d6e6f83ee73cd1d5058a64fa254
Jingshi Sun <hanzi@utexas.edu>
2016-04-06 01:01:00 -0500

    automatic message

--- ned+_model.py
+++ ned+_model.py
@@ -192,6 +192,13 @@ def max_vacation_slot_rule(model, t):
 	return lhs <= get_slot(t)  
 model.pilot_vacation_slot_exceed = pe.Constraint(model.time, rule = max_vacation_slot_rule)
 
+def trainer_location_rule(model, p, r, f, b, t):
+	if p in model.trainer_pilots:
+		return model.T[p, b, t] <= model.Y[p,r,f,b,t]
+	else:
+		return pe.Constraint.Skip
+model.trainer_location = pe.Constraint(variable_set*model.time, rule=trainer_location_rule)
+
 def trainee_var_binding_rule(model, p, r, f, b, t):
 	if(p in fleet_change):
 		return model.Y[p,r,f,b,t] - model.Y[p,r,f,b,t+1] - model.Trainee[p, b, t] == 0
