980b5d5c4dac82cb46cf79fe38bdbb4940b1d05b
whalejasmine <hslzjy@gmail.com>
2016-04-19 19:40:22 -0500

    new

--- script.sh
+++ script.sh
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-#! /opt/local/bin/expect -f
-=======
 #! /usr/bin/expect -f
->>>>>>> Stashed changes
 spawn ssh js79735@me-dimitrovresearch.engr.utexas.edu
 expect -re "assword: *$"
 sleep 1
