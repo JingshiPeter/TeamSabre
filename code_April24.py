4905ec20bbcc6977b6187559534fb6a9ca67c0af
whalejasmine <hslzjy@gmail.com>
2016-04-27 16:19:10 -0500

    add

--- code_April24.py
+++ code_April24.py
@@ -26,6 +26,23 @@ def get_nonfix_pilots():
 def get_all_pilots():
 	return set(crew_df[crew_df.Rank != "SIM_INS"]['Crew_ID'])
 
+def get_vacation(model, p, t):
+	vacations = {
+		'900201' : [2,3,7,8],
+		'900488' : [3,4,14,15],
+		'900387' : [4,7],
+		'900369' : [4,5,6],
+		'800000' : [2,3],
+		'700125' : [11]
+	}
+	pilot = str(p)
+	if pilot in vacations:
+		if t+1 in vacations[pilot]:
+			return 300
+		else:
+			return 0
+	else:
+		return 0
 ####trainer Pilots
 trainers = set(crew_df[(crew_df.Instructor == "TR3233_1")]['Crew_ID'])
 
@@ -117,18 +134,18 @@ for pilot in fixed_df['Crew_ID'].values:
 
 
 model.nonfix_pilots = pe.Set(initialize = nonfixed_df['Crew_ID'].values)
+model.fix_pilots = model.pilots - model.nonfix_pilots
+
 model.nonfix_var_set = pe.Set(initialize = nonfix_var_set)
 model.fix_var_set = pe.Set(initialize = fix_var_set)
 model.all_var_set = pe.Set(initialize = all_var_set)
-model.fix_pilots = model.pilots - model.nonfix_pilots
-print "Number of nonfix_pilots is " + str(len(nonfixed_df['Crew_ID'].values))
+
 model.trainer_pilots = pe.Set(initialize = trainers)
 model.rank_pilots = pe.Set(initialize = rank_change)
 model.fleet_pilots = pe.Set(initialize = fleet_change)
 model.base_pilots = pe.Set(initialize = base_change)
 model.from_pos = pe.Set(initialize = from_set)
 model.to_pos = pe.Set(initialize = to_set)
-model.all_pos = pe.Set(initialize = nonfix_var_set)
 #new set
 nonfixed_trainer=[]
 for pilot in nonfixed_df['Crew_ID'].values:
@@ -146,31 +163,31 @@ model.rank = pe.Set(initialize=['CPT','FO'])
 model.fleet = pe.Set(initialize=['A330','A320'])
 model.base = pe.Set(initialize=[1,2])
 model.time = pe.Set(initialize=range(26))
+model.train_start_time = pe.Set(initialize=range(24))
 model.timestart = pe.Set(initialize=range(25))
 model.quarterstart = pe.Set(initialize = [0,13])
 
 model.Y = pe.Var(model.nonfix_var_set*model.time, domain=pe.Binary)
 # this variable contained all pilots
 model.Yall = pe.Var(model.all_var_set*model.time, domain=pe.Binary)
-model.Yonwork = pe.Var(model.all_var_set*model.time, domain=pe.Binary)
+model.Ynowork = pe.Var(model.all_var_set*model.time, domain=pe.Binary)
 model.shortage = pe.Var(model.rank*model.fleet*model.base*model.time, domain = pe.NonNegativeIntegers)
 model.surplus = pe.Var(model.rank*model.fleet*model.base*model.time, domain = pe.NonNegativeIntegers)
-model.T = pe.Var(model.trainer_pilots*model.base*model.time, domain=pe.Binary)
+model.Trainer = pe.Var(model.trainer_pilots*model.base*model.time, domain=pe.Binary)
 model.Trainee = pe.Var(model.fleet_pilots*model.base*model.time, domain=pe.Binary)
 model.V = pe.Var(model.pilots*model.time, domain=pe.Binary)
 model.VP = pe.Var(model.pilots*model.quarterstart, domain=pe.NonNegativeIntegers)
 #only nonfix pilots can take vacation or training?
-model.Vposition = pe.Var(model.nonfix_var_set*model.time, domain=pe.Binary)
+model.Vnonfix_position = pe.Var(model.nonfix_var_set*model.time, domain=pe.Binary)
 model.Vfix_position = pe.Var(model.fix_var_set*model.time, domain=pe.Binary)
-model.Tposition = pe.Var(model.trainer_pilots*model.rank*model.fleet*model.base*model.time, domain=pe.Binary)
-model.Trainee_po = pe.Var(model.fleet_pilots*model.rank*model.fleet*model.base*model.time, domain=pe.Binary)
+model.Trainer_pos = pe.Var(model.trainer_pilots*model.rank*model.fleet*model.base*model.time, domain=pe.Binary)
+model.Trainee_pos = pe.Var(model.fleet_pilots*model.rank*model.fleet*model.base*model.time, domain=pe.Binary)
 model.VS = pe.Var(model.pilots*model.time, domain = pe.NonNegativeIntegers)
 
 model.short_cost = pe.Param(model.rank*model.fleet*model.base*model.time, initialize = 70000)
-model.normal_cost = pe.Param(model.nonfix_var_set*model.time, initialize = 3500)
 model.base_transition_cost = pe.Param(model.nonfix_var_set*model.time, initialize = 15000)
 model.fleet_transition_cost = pe.Param(model.nonfix_var_set*model.time, initialize = 5000)
-model.vacation_penalty = pe.Param(model.pilots*model.quarterstart, initialize = 3000)
+model.vacation_penalty = pe.Param(model.pilots*model.quarterstart, initialize = 300)
 model.seniority_reward = pe.Param(model.pilots*model.time, initialize = 50)
 
 
@@ -215,15 +232,8 @@ def daily_cost(model, p, rank, fleet, base,time):
                 return 600*1.1*1.1*1.1                    
 
 model.dailycost = pe.Param(model.all_var_set*model.time, initialize = daily_cost)
-#new constraints 1-5
-#non-fixed only
-#def trainer_rule(model,p,b,t):
-#	rhs = 0
-#	for f in model.fleet:
-#		for r in model.rank:
-#			rhs=rhs+model.Y[p,r,f,b,t]
-#	return model.T[p,b,t] <= rhs
-#model.trainer_constraint = pe.Constraint(model.trainer_nonfix_pilots*model.base*model.time,rule=trainer_rule)
+model.vacation_reward = pe.Param(model.pilots*model.time, initialize = get_vacation)
+
 
 #include fixed
 def trainer_rule(model,p,b,t):
@@ -232,20 +242,20 @@ def trainer_rule(model,p,b,t):
 		for r in model.rank:
 			if (p,r,f,b) in model.all_var_set:
 				rhs=rhs+model.Yall[p,r,f,b,t]
-	return model.T[p,b,t] <= rhs
+	return model.Trainer[p,b,t] <= rhs
 model.trainer_constraint = pe.Constraint(model.trainer_pilots*model.base*model.time,rule=trainer_rule)
 #def trainer_rule_onwork(model,p,b,t):
 #	rhs = 0
 #	for f in model.fleet:
 #		for r in model.rank:
 #			if (p,r,f,b) in model.all_var_set:
-#				rhs=rhs+model.Yonwork[p,r,f,b,t]
-#	return rhs <= model.T[p,b,t]
+#				rhs=rhs+model.Ynowork[p,r,f,b,t]
+#	return rhs <= model.Trainer[p,b,t]
 #model.trainer_constraintononwork = pe.Constraint(model.trainer_pilots*model.base*model.time,rule=trainer_rule_onwork)
 ##
 
 def pilot_on_work1(model, p, r, f, b,t):
-    return  model.Yonwork[p,r,f,b,t] <= model.Yall[p,r,f,b,t]
+    return  model.Ynowork[p,r,f,b,t] <= model.Yall[p,r,f,b,t]
 model.pilotonwork1 = pe.Constraint(model.all_var_set*model.time,rule=pilot_on_work1)
 ##
 def trainee_rule(model,p,b,t):
@@ -256,16 +266,9 @@ def trainee_rule(model,p,b,t):
 				rhs=rhs+model.Y[p,r,f,b,t]	
 	return model.Trainee[p,b,t] <= rhs
 model.trainee_constraint = pe.Constraint(model.fleet_pilots*model.base*model.time,rule=trainee_rule)
-#def trainee_rule_onwork(model,p,b,t):
-#	rhs=0
-#	for f in model.fleet:
-#		for r in model.rank:
-#			if (p,r,f,b) in model.nonfix_var_set:
-#				rhs=rhs+model.Yonwork[p,r,f,b,t]	
-#	return rhs <= model.Trainee[p,b,t]
-#model.trainee_constraint_onwork = pe.Constraint(model.fleet_pilots*model.base*model.time,rule=trainee_rule_onwork)
+
 def vacation_rule1(model,p,b,t):
-	return model.V[p,t] <= 1- model.T[p,b,t]
+	return model.V[p,t] <= 1- model.Trainer[p,b,t]
 model.vacation_constraint1 = pe.Constraint(model.trainer_pilots*model.base*model.time,rule=vacation_rule1)
 def vacation_rule2(model,p,b,t):
 	return model.V[p,t] <= 1- model.Trainee[p,b,t]
@@ -273,31 +276,33 @@ model.vacation_constraint2 = pe.Constraint(model.fleet_pilots*model.base*model.t
 #include fixed
 def training_rule(model,p,r,f,b,t):
 	if (p,r,f,b) in model.all_var_set:
-		return model.Tposition[p,r,f,b,t] >= model.T[p,b,t] + model.Yall[p,r,f,b,t]-1
+		return model.Trainer_pos[p,r,f,b,t] >= model.Trainer[p,b,t] + model.Yall[p,r,f,b,t]-1
 	else:
 		return pe.Constraint.Skip
 model.training_constraint = pe.Constraint(model.trainer_pilots*model.rank*model.fleet*model.base*model.time,rule = training_rule)
 #
 def training_rule_onwork(model,p,r,f,b,t):
 	if (p,r,f,b) in model.all_var_set:
-		return model.Yonwork[p,r,f,b,t] <= model.Tposition[p,r,f,b,t]
+		return model.Ynowork[p,r,f,b,t] <= model.Trainer_pos[p,r,f,b,t]
 	else:
 		return pe.Constraint.Skip
 model.training_constraint_onwork = pe.Constraint(model.trainer_pilots*model.rank*model.fleet*model.base*model.time,rule = training_rule_onwork)
-#
-#non-fixed only
-#def training_rule(model,p,r,f,b,t):
-#	return model.Tposition[p,r,f,b,t] >= model.T[p,b,t] + model.Y[p,r,f,b,t]-1
-#model.training_constraint = pe.Constraint(model.trainer_nonfix_pilots*model.rank*model.fleet*model.base*model.time)
+
 def trainee_rule2(model,p,r,f,b,t):
-	if (p,r,f,b) in model.all_var_set:
-		return model.Trainee_po[p,r,f,b,t] >= model.Trainee[p,b,t] +model.Y[p,r,f,b,t] -1
+	if p in model.fleet_pilots:
+		if(t >= 2):
+			return model.Trainee_pos[p,r,f,b,t] >= model.Trainee[p,b,t] + model.Trainee[p,b,t-1] + model.Trainee[p,b,t-2] +model.Y[p,r,f,b,t] -1
+		elif(t >= 1):
+			return model.Trainee_pos[p,r,f,b,t] >= model.Trainee[p,b,t] + model.Trainee[p,b,t-1]+model.Y[p,r,f,b,t] -1
+		else:
+			return model.Trainee_pos[p,r,f,b,t] >= model.Trainee[p,b,t] + model.Y[p,r,f,b,t] -1
 	else:
 		return pe.Constraint.Skip
-model.trainee_constraint2 = pe.Constraint(model.fleet_pilots*model.rank*model.fleet*model.base*model.time, rule = trainee_rule2)
+model.trainee_constraint2 = pe.Constraint(model.nonfix_var_set*model.time, rule = trainee_rule2)
+
 def trainee_rule2_onwork(model,p,r,f,b,t):
 	if (p,r,f,b) in model.all_var_set:
-		return  model.Yonwork[p,r,f,b,t] <= model.Trainee_po[p,r,f,b,t]
+		return  model.Ynowork[p,r,f,b,t] <= model.Trainee_pos[p,r,f,b,t]
 	else:
 		return pe.Constraint.Skip
 model.trainee_constraint2_onwork = pe.Constraint(model.fleet_pilots*model.rank*model.fleet*model.base*model.time, rule = trainee_rule2_onwork)
@@ -306,21 +311,21 @@ def demand_rule(model,r,f,b,t):
 	vp=0
 	for p in model.nonfix_pilots :
 		if (p, r, f, b) in model.nonfix_var_set:
-			vp +=model.Vposition[p, r, f, b, t]
+			vp +=model.Vnonfix_position[p, r, f, b, t]
 
 	tp=0
 	for p in model.trainer_pilots :
 		if (p, r, f, b) in model.all_var_set:
-			vp +=model.Tposition[p, r, f, b, t]
+			tp +=model.Trainer_pos[p, r, f, b, t]
 
 	traineep=0
 	for p in model.fleet_pilots :
 		if (p, r, f, b) in model.nonfix_var_set:
-			vp +=model.Trainee_po[p, r, f, b, t]
+			traineep +=model.Trainee_pos[p, r, f, b, t]
 	vfixp=0
 	for p in model.fix_pilots :
 		if (p, r, f, b) in model.fix_var_set:
-			vp +=model.Vfix_position[p, r, f, b, t]
+			vfixp +=model.Vfix_position[p, r, f, b, t]
 
 	curr_fixed = fixed_df[(fixed_df.Rank==r)&(fixed_df.Cur_Fleet==f)&(fixed_df.Current_Base==b)]['Crew_ID'].values
 	pilot = len(curr_fixed)
@@ -332,36 +337,6 @@ def demand_rule(model,r,f,b,t):
 	demand = get_demand(r,f,b,t)
 	return rhs == demand 
 model.demand_constraint = pe.Constraint(model.rank*model.fleet*model.base*model.time, rule = demand_rule)
-'''
-# demand rule
-#TODO: need to figure out vacation, trainer cases.
-
-def demand_rule(model,r,f,b,t):
-	curr_fixed = fixed_df[(fixed_df.Rank==r)&(fixed_df.Cur_Fleet==f)&(fixed_df.Current_Base==b)]['Crew_ID'].values
-	rhs = len(curr_fixed)
-	for p in curr_fixed:
-		if(p in model.trainer_pilots):
-			rhs -= model.T[p, b ,t]
-	for p in curr_fixed:
-		# if p is in vacation
-		rhs -= model.V[p,t]
-
-	for p in toPos[(toPos.RANK==r)&(toPos.FLEET==f)&(toPos.BASE==b)]['ID'].values:
-		rhs += model.Y[p,r,f,b,t]
-		# if p come to the position by a fleet change, it takes 5 weeks training
-		if(p in fleet_change and t >= 5) :
-			rhs = rhs - 1 + model.Y[p,r,f,b,t-5]
-		elif(p in fleet_change and t < 5) :
-			rhs = rhs - model.Y[p,r,f,b,t]
-
-	for p in fromPos[(fromPos.RANK==r)&(fromPos.FLEET==f)&(fromPos.BASE==b)]['ID'].values:
-		rhs = rhs - model.Y[p,r,f,b,t]
-
-	rhs = rhs + model.shortage[r,f,b,t] - model.surplus[r,f,b,t]
-	return rhs == get_demand(r,f,b,t)
-
-model.Demand = pe.Constraint(model.rank*model.fleet*model.base*model.time, rule=demand_rule)
-'''
 # model.Demand.pprint()
 
 #at time t, a pilot should occupy one and only one position
@@ -436,14 +411,15 @@ def seniority_rule(model,p,t):
 	return lhs == 0
 model.seniority = pe.Constraint(model.pilots*model.time, rule = seniority_rule)
 
-### if the pilot p is not at position [b,f,r]at week t, even if he is on vacation, then Vposition[p,b,f,r,t] = 0
+### if the pilot p is not at position [b,f,r]at week t, even if he is on vacation, then Vnonfix_position[p,b,f,r,t] = 0
 def vacation_position_rule(model,p,r,f,b,t):
 	lhs = 0
-	lhs = model.V[p,t] + model.Y[p,r,f,b,t] - 1 - model.Vposition[p,r,f,b,t]
+	lhs = model.V[p,t] + model.Y[p,r,f,b,t] - 1 - model.Vnonfix_position[p,r,f,b,t]
 	return lhs <= 0
 model.Vacation_position = pe.Constraint(model.nonfix_var_set*model.time, rule = vacation_position_rule)
+
 def vacation_position_rule_onwork(model,p,r,f,b,t):
-	return model.Yonwork[p,r,f,b,t] <= model.Vposition[p,r,f,b,t]
+	return model.Ynowork[p,r,f,b,t] <= model.Vnonfix_position[p,r,f,b,t]
 model.Vacation_position_onwork = pe.Constraint(model.nonfix_var_set*model.time, rule = vacation_position_rule_onwork)
 
 def vacation_position_rule2(model,p,r,f,b,t):
@@ -451,24 +427,10 @@ def vacation_position_rule2(model,p,r,f,b,t):
 	lhs = model.V[p,t] + model.Yall[p,r,f,b,t] - 1 - model.Vfix_position[p,r,f,b,t]
 	return lhs <= 0
 model.Vacation_position2 = pe.Constraint(model.fix_var_set*model.time, rule = vacation_position_rule2)
+
 def vacation_position_rule2_onwork(model,p,r,f,b,t):
-	return model.Yonwork[p,r,f,b,t] <= model.Vfix_position[p,r,f,b,t]
+	return model.Ynowork[p,r,f,b,t] <= model.Vfix_position[p,r,f,b,t]
 model.Vacation_position2_onwork = pe.Constraint(model.fix_var_set*model.time, rule = vacation_position_rule2_onwork)
-
-
-# def trainer_location_rule(model, p, r, f, b, t):
-# 	if p in model.trainer_pilots:
-# 		return model.T[p, b, t] <= model.Y[p,r,f,b,t]
-# 	else:
-# 		return pe.Constraint.Skip
-# model.trainer_location = pe.Constraint(model.all_pos*model.time, rule=trainer_location_rule)
-
-# def trainee_location_rule(model, p, r, f, b, t):
-# 	if p in model.fleet_pilots:
-# 		return model.Trainee[p, b, t] <= model.Y[p,r,f,b,t]
-# 	else:
-# 		return pe.Constraint.Skip
-# model.trainee_location = pe.Constraint(model.all_pos*model.time, rule=trainee_location_rule)
 
 def trainee_var_binding_rule(model, p, r, f, b, t):
 	if(p in fleet_change):
@@ -480,12 +442,12 @@ model.trainee_var_binding = pe.Constraint(model.from_pos*model.timestart, rule=t
 def trainee_trainer_rule(model, b, t):
 	total_trainer = 0
 	for p in model.trainer_pilots:
-		total_trainer += model.T[p, b, t]
+		total_trainer += model.Trainer[p, b, t+2]
 	total_trainee = 0
 	for p in fleet_change:
 		total_trainee += model.Trainee[p, b, t]
 	return total_trainer == total_trainee
-model.trainee_trainer = pe.Constraint(model.base*model.time, rule = trainee_trainer_rule)
+model.trainee_trainer = pe.Constraint(model.base*model.train_start_time, rule = trainee_trainer_rule)
 
 ###Yall and Y binding rule (for non-fix pilot part)
 def yall_y_binding_rule(model, p, r, f, b, t):
@@ -503,8 +465,6 @@ model.yall_setting = pe.Constraint(model.fix_var_set*model.time, rule = yall_set
 
 
 ###OBJ###
-###Normal Operation:
-model.total_normal_cost = pe.summation(model.normal_cost, model.Y)
 ###Transitions:
 model.total_fleet_trans_cost = pe.summation(model.fleet_transition_cost, model.Y, index = [(p, r, f, b, 25) for(p, r, f, b) in model.to_pos if p in model.fleet_pilots ])
 model.total_base_trans_cost = pe.summation(model.base_transition_cost, model.Y, index = [(p, r, f, b, 25) for(p, r, f, b) in model.to_pos if p in model.base_pilots ])
@@ -514,12 +474,14 @@ model.total_shortage_cost = pe.summation(model.short_cost, model.shortage)
 ###Vacation Penalty:
 model.total_vacation_penalty = pe.summation(model.vacation_penalty, model.VP)
 model.total_seniority_reward = pe.summation(model.seniority_reward, model.VS)
+###Vacation reward:
+model.total_vacation_reward = pe.summation(model.vacation_reward,model.V)
 
 ###Daily operation cost:
 model.operationcost = pe.summation(model.dailycost,model.Yall)
-model.operationminus = pe.summation(model.dailycost,model.Yonwork)
+model.operationminus = pe.summation(model.dailycost,model.Ynowork)
 
-model.OBJ = pe.Objective(expr = model.total_shortage_cost + model.total_trans_cost + model.total_normal_cost + model.total_vacation_penalty + model.operationcost - model.operationminus - model.total_seniority_reward, sense=pe.minimize)
+model.OBJ = pe.Objective(expr = model.total_shortage_cost + model.total_trans_cost + model.total_vacation_penalty + model.operationcost - model.operationminus - model.total_seniority_reward - model.total_vacation_reward, sense=pe.minimize)
 solver = pyomo.opt.SolverFactory('cplex')
 
 
@@ -549,7 +511,7 @@ print "\nTotal number of TR3233_1 qualified trainers is " + str(len(model.traine
 for p in model.trainer_pilots:
 	for t in model.timestart:
 		for b in model.base:
-			if model.T[p, b, t].value == 1 :
+			if model.Trainer[p, b, t].value == 1 :
 				print "trainer " + p + " is training at week " + str(t)	+ " at base " + str(b)
 
 
@@ -567,15 +529,15 @@ for (p, r, f, b) in model.fix_var_set:
 		if(model.Vfix_position[p, r, f, b, t].value == 1):
 			print p +" "+str(t) + " Vacation"
 		if(p in model.trainer_pilots):
-			if(model.T[p,b,t].value == 1):
+			if(model.Trainer[p,b,t].value == 1):
 				print p +" "+str(t) + " Giving Training"
 
 for (p, r, f, b) in model.nonfix_var_set:
 	for t in model.time:
-		if(model.Vposition[p, r, f, b, t].value == 1):
+		if(model.Vnonfix_position[p, r, f, b, t].value == 1):
 			print p +" "+str(t) + " Vacation"
 		if(p in model.trainer_pilots):
-			if(model.T[p,b,t].value == 1):
+			if(model.Trainer[p,b,t].value == 1):
 				print p +" "+str(t) + " Giving Training"
 		if(p in model.fleet_pilots):
 			if(model.Trainee[p,b,t].value == 1):
@@ -587,14 +549,13 @@ for (p, r, f, b) in model.nonfix_var_set:
 			if((model.Y[p, r, f, b, t].value == 1) & (model.Y[p, r, f, b, t+1].value == 0)):
 				print p +" "+str(t) + " Rank change from " + str(r)
 
-
 print '\nTotal cost = ', model.OBJ()
 print 'Shortage cost is = ', model.total_shortage_cost()
 print 'Transition cost is = ', model.total_trans_cost()
-print 'Normal Operation cost is = ', model.total_normal_cost()
 print 'Vacation Penalty is = ', model.total_vacation_penalty()
 print 'Seniority Reward is =', model.total_seniority_reward()
 print 'Daily Salary Cost is =', model.operationcost()
 print 'Minus cost is =', model.operationminus()
+print 'Vacation reward is =', model.total_vacation_reward()
 #instance.solutions.load_from(results)
 #model.solutions.load_from(results)
