8dccc897947601947cf7f4f47346065741dc61eb
whalejasmine <hslzjy@gmail.com>
2016-05-05 16:09:12 -0500

    Update code_May5.py

--- code_May5.py
+++ code_May5.py
@@ -555,3 +555,134 @@ print 'Vacation  Reward is =', model.total_vacation_reward()
 print 'Seniority Reward is =', model.total_seniority_reward()
 #instance.solutions.load_from(results)
 #model.solutions.load_from(results)
+
+
+##################create csv for results##############
+
+import pandas as pd
+import csv
+import numpy as np
+
+
+# create new dataframe
+
+# columns =['pilots_id', 'status', 'trasition', 'time', 'time_vacation']
+columns =[ '1', '2', '3', '4','5','6','7','8','9','10','11','12','13','14','15','16',
+'7','18','19','20','21','22','23','24','25','26']
+status_df = pd.DataFrame(index = crew_df['Crew_ID'], columns = columns)
+
+status_df = status_df.fillna(0)
+
+#status_df['pilots_id'] = crew_df['Crew_ID']
+
+
+trainer_list = []
+trainee_list = []
+vacation_list = []
+transition_list = []
+
+# Trainer = 1
+
+for p in model.trainer_pilots:
+	for t in model.timestart:
+		for b in model.base:
+			if model.Trainer[p, b, t].value == 1 :
+				#trainer_list.append((p, t, b))
+				#status_df.set_value(status_df.pilots_id == p,'status',1)
+				#status_df.set_value(status_df.pilots_id == p,'time',t)
+				#status_df.set_value(status_df.pilots_id == p, status_df.columns.values[t] == str(t) ,1)
+				status_df.ix[p, t]= 1
+
+
+
+#trainer = pd.DataFrame(trainer_list)
+#trainer.columns = ['ID','Time','Base']
+
+# Trainee = 2
+for p in model.fleet_pilots:
+	for t in model.timestart:
+ 		for b in model.base:
+ 			if model.Trainee[p, b, t].value == 1 :
+ 				#trainee_list.append((p, t, b))
+ 				#status_df.set_value(status_df.pilots_id == p,'status',2)
+				#status_df.set_value(status_df.pilots_id == p,'time',t)
+				#status_df.set_value(status_df.pilots_id == p, status_df.columns.values ==t ,2)
+				status_df.ix[p, t]= 2
+
+#trainee = pd.DataFrame(trainee_list)
+#trainee.columns = ['ID','Time','Base']
+
+# Vacation = 3
+
+for p in model.pilots:
+	for t in model.timestart:
+		if model.V[p, t].value == 1:
+			#vacation_list.append((p, t))
+			#status_df.set_value(status_df.pilots_id == p,'status_vacation',3)
+			#status_df.set_value(status_df.pilots_id == p,'time_vacation',t)
+
+			#status_df.set_value(p, t, 3)
+			status_df.ix[p, t] = 3
+
+
+#vacation = pd.DataFrame(vacation_list)
+#vacation.columns = ['ID','Time']
+
+
+# Trasition
+# b1 = 4 b2 = 5 'CPT'=6 'FO'=7
+for (p, r, f, b) in model.nonfix_var_set:
+	for t in model.time:
+		if((p in model.base_pilots) & (t in model.timestart) & ((p,r,f,b) in model.from_pos)):
+			if((model.Y[p, r, f, b, t].value == 1) & (model.Y[p, r, f, b, t+1].value == 0)):
+				#transition_list.append((p, t, 'b'+str(b)))
+				if b == 1:
+					status_df.ix[p, t] = 4
+				if b == 2:
+					status_df.ix[p, t] = 5
+
+for (p, r, f, b) in model.nonfix_var_set:
+	for t in model.time:
+		if((p in model.rank_pilots) & (t in model.timestart) & ((p,r,f,b) in model.from_pos)):
+			if((model.Y[p, r, f, b, t].value == 1) & (model.Y[p, r, f, b, t+1].value == 0)):
+				#transition_list.append((p, t, str(r)))
+				if r == 'CPT':
+					status_df.ix[p, t] = 6
+				if r == 'FO':
+					status_df.ix[p, t] = 7
+
+# csv
+status_df.to_csv("status.csv",index = True)
+
+
+
+
+# Shortage
+
+shortage_list =[]
+surplus_list =[]
+
+for b in model.base:
+	for r in model.rank:
+		for f in model.fleet:
+			for t in model.time:
+				if model.shortage[r,f,b,t].value > 0:
+					shortage_list.append((b,str(r)+str(f),t,round(model.shortage[r,f,b,t].value,0)))
+				if model.shortage[r,f,b,t].value == 0:
+					shortage_list.append((b,str(r)+str(f),t,0))
+
+				
+for b in model.base:
+	for r in model.rank:
+		for f in model.fleet:
+			for t in model.time:
+					surplus_list.append((b,str(r)+str(f),t,model.surplus[r,f,b,t].value))
+				
+
+shortage = pd.DataFrame(shortage_list)
+surplus = pd.DataFrame(surplus_list)
+
+# csv
+shortage.to_csv("shortage.csv",index = True)
+surplus.to_csv("surplus.csv",index = True)
+
